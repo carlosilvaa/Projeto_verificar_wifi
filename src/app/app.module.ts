4db22b2129218f0a96de3f8790f1d8bce7f0f12d
Maria Eduarda Macedo <mdudu2213@gmail.com>
2024-06-07 00:07:17 -0300

    feat: add graficos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,7 @@ import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { WifiDataComponent } from './pages/wifi-data/wifi-data.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GraficsComponetComponent } from './pages/graphics/graphics-componet.component';
-import { GraphsComponent } from './_components/graphs/graphs.component';
-import { DxButtonModule, DxChartModule } from 'devextreme-angular';
-import { DxiSeriesModule } from 'devextreme-angular/ui/nested';
-
+import { DxChartModule } from 'devextreme-angular';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,16 +19,13 @@ import { DxiSeriesModule } from 'devextreme-angular/ui/nested';
     FooterComponent,
     BaseUiComponent,
     WifiDataComponent,
-    GraficsComponetComponent,
-    GraphsComponent,
+    GraficsComponetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    DxButtonModule,
-    DxChartModule,
-    DxiSeriesModule
+    DxChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/graphics/graphics-componet.component.ts
+++ src/app/pages/graphics/graphics-componet.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Data } from '@angular/router';
+import { DataService } from './../../_services/data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grafics-componet',
   templateUrl: './graphics-componet.component.html',
   styleUrl: './graphics-componet.component.css'
 })
-export class GraficsComponetComponent {
-  
+export class GraficsComponetComponent implements OnInit {
+
+  public dataSource: Data[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataSource = this.dataService.getData();
+  }
+
 }
